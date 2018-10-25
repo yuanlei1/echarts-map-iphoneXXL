2ebdf1fa2ae82cdf3582d094d8526bd122bf10b5
yuanlei1 <821271898@qq.com>
2018-10-25 22:22:55 +0800

    init

---
+++ project/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>          
+    <div>
+        <div id="mapContainer" style="width:600px; height:600px"></div>
+    </div>
+</template>
+        
+<script>
+    import option from './map-option'  //引入配置项
+    require('./../../node_modules/echarts/map/js/china.js')  //引入china.js地图文件
+
+    export default {
+        name: 'hello',
+        data () {
+          return {  
+          }
+        },
+        methods: {
+          drawChinaMap() {
+            var myChart = this.$echarts.init(document.getElementById('mapContainer'));
+            myChart.setOption(option);
+          }
+        },
+        mounted() {
+          this.drawChinaMap();
+        }
+    }
+</script>
+        
+<style scoped>
+       
+</style>
